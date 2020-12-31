6987a3da2aec0682e373270ef455c34a8314aef4
Jesse C. Yentsch <yentschj@gmail.com>
2020-12-31 18:09:14 -0500

    Created the ability to edit products

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -4,17 +4,23 @@ const {
   getProducts,
   getProductById,
   deleteProduct,
+  updateProduct,
+  createProduct,
 } = require('../controllers/productController')
 const { protect, admin } = require('../middleware/authMiddleware')
 
 //@desc Fetch all products
 //@route GET /api/products
 //@access Public route
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
 //@desc Fetch single product
 //@route GET /api/products/:id
 //@access Public
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 module.exports = router
