93f215cfccedff931c46a99468e7fb1b948c246c
Jesse C. Yentsch <yentschj@gmail.com>
2021-01-01 20:45:18 -0500

    Add admin ability to mark as delivered

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -53,8 +53,6 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 })
 
-module.exports = { addOrderItems, getOrderById }
-
 //@desc Update order to paid
 //@route GET /api/orders/:id/pay
 //@access Private
@@ -70,6 +68,25 @@ const updateOrderToPaid = asyncHandler(async (req, res) => {
       update_time: req.body.update_time,
       email_address: req.body.payer.email_address,
     }
+
+    const updatedOrder = await order.save()
+
+    res.json(updatedOrder)
+  } else {
+    res.status(404)
+    throw new Error('Order not found')
+  }
+})
+
+//@desc Update order to delivered
+//@route GET /api/orders/:id/deliver
+//@access Private/Admin
+const updateOrderToDelivered = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id)
+
+  if (order) {
+    order.isDelivered = true
+    order.deliveredAt = Date.now()
 
     const updatedOrder = await order.save()
 
@@ -102,6 +119,7 @@ module.exports = {
   addOrderItems,
   getOrderById,
   updateOrderToPaid,
+  updateOrderToDelivered,
   getMyOrders,
   getOrders,
 }
