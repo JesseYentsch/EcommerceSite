90666b2fadc6bbe1ed42f98e7c990317aec83a80
Jesse C. Yentsch <yentschj@gmail.com>
2021-01-04 14:50:23 -0500

    Updated package.json

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,10 @@ const dirname = path.resolve()
 app.use('/uploads', express.static(path.join(dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
   app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '..', 'frontend', 'build', 'index.html'))
   )
 } else {
   app.get('/', (req, res) => {
