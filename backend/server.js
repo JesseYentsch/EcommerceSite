ba7871ea421f65217fb3a2740fd955b5632f723b
Jesse C. Yentsch <yentschj@gmail.com>
2021-01-03 20:16:22 -0500

    Changed dirname

--- backend/server.js
+++ backend/server.js
@@ -35,11 +35,12 @@ const dirname = path.resolve()
 app.use('/uploads', express.static(path.join(dirname, '/uploads')))
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(dirname, '../frontend/build')))
+  app.use(express.static(path.join(__dirname, '../frontend/build')))
 
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(dirname, '../frontend/build/index.html'))
-  )
+  app.get('*', (req, res) => {
+    console.log(dirname)
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })
 } else {
   app.get('/', (req, res) => {
     res.send('API is running...')
